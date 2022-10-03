ba4c69ce1ecd8109b082a75b6a10454b95fe02a6
Ananya Garg <agarg.ananya@gmail.com>
2022-10-03 17:16:22 -0400

    styling and layout

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-import "node-fetch"
+fetch("https://pokeapi.co/api/v2/pokemon/1").then((resp) => resp.json()).then((data)=> console.log(data));
+
+// fields needed: height, weight, hp, attack, defense, special-attack, special-defense, speed, image, types, moves
+
+
+
